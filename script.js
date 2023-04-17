82cd707d7f4b0c15c309936bc89dcb3779e5d1f9
Aguiar425 <aguiar425@gmail.com>
2023-04-17 16:09:00 +0100

    inner fucntions

--- script.js
+++ script.js
@@ -1,4 +1,79 @@
-function duckMoveRight() {
+function horizontal(direction) {
+    let duck =  document.getElementById("duck");
+       if(direction == "left") {
+        //duck.removeAttribute("class");
+        duck.setAttribute("class","move-Left");
+        duck.style.transform = "scaleX(-1)";
+       }
+       if(direction == "right") {
+         //duck.removeAttribute("class");
+         duck.setAttribute("class","move-right");
+         //duck.style.transform = "none";
+       }
+
+    const start =  () => {duck.style.backgroundPosition = `-130px -121px`;
+    duck.style.width = "34px";
+    duck.style.height = "24px";
+   }
+   const mid = () => {duck.style.backgroundPosition = `-170px -123px`;
+   duck.style.width = "34px";
+   duck.style.height = "20px";
+   }
+   const final = () => {duck.style.backgroundPosition = `-211px -119px`;
+   duck.style.width = "32px";
+   duck.style.height = "28px";
+   }
+   setInterval(() => {
+       start();
+       setTimeout(() => {
+         mid();
+       }, 300);
+       setTimeout(() => {
+         final();
+       }, 600);
+     }, 900);
+     setInterval();
+   }
+
+   function diagonal(x,y) {
+    let duck =  document.getElementById("duck");
+       if(x == "left") {
+           duck.style.transform = "scaleX(-1)";
+       }
+       if (y == "down") {
+           duck.style.transform = "scaleY(-1);";
+       }
+       
+    const start =  () => {duck.style.backgroundPosition = `-134px -157px`;
+    duck.style.width = "25px";
+    duck.style.height = "31px";
+   }
+
+   const mid = () => {duck.style.backgroundPosition = `-171px -158px`;
+   duck.style.width = "32px";
+   duck.style.height = "29px";
+   }
+
+   const final = () => {duck.style.backgroundPosition = `-213px -157px`;
+   duck.style.width = "27px";
+   duck.style.height = "31px";
+   }
+
+   setInterval(() => {
+       start();
+       setTimeout(() => {
+         mid();
+       }, 250);
+       setTimeout(() => {
+         final();
+       }, 400);
+     }, 550);
+     setInterval();
+   }
+   
+
+   //-------------------------------------------------------------------------------------
+function duckMoveHorizontal() {
     setInterval(function() {
         setTimeout(horizontalRightTwo, 250);
         setTimeout(horizontalRightThree, 400);
@@ -6,19 +81,19 @@ function duckMoveRight() {
       }, 500);
     }
 
-function horizontalRightOne(){
+function horizontalOne(){
     let duck = document.getElementById("duck").style.backgroundPosition = `-130px -121px`;
     duck.style.width = "34px";
     duck.style.height = "20px";
 }
 
-function horizontalRightTwo(){
+function horizontalTwo(){
     let duck = document.getElementById("duck").style.backgroundPosition = `-170px -123px`;
     duck.style.width = "34px";
     duck.style.height = "20px";
 }
 
-function horizontalRightThree(){
+function horizontalThree(){
     let duck = document.getElementById("duck").style.backgroundPosition = `-211px -119px`;
     duck.style.width = "34px";
     duck.style.height = "20px";
@@ -79,7 +154,7 @@ function upThree(){
 function hidePopup(){
     const popup = document.querySelector(".popup-container");
     popup.classList.add("hide-container");
-    duckMoveRight();
+    horizontal("left");
 }
 // let tID; 
 // let position = 
