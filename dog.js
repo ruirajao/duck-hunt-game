cfa68985013ed0749c327423a5ad99e6c448215a
Rui Rajão <122367154+ruirajao@users.noreply.github.com>
2023-04-20 17:53:49 +0100

    dog class

---
+++ dog.js
@@ -0,0 +1,54 @@
+class Dog {
+
+    constructor(id) {
+        this.dogId = `${id}`;
+        this.fieldContainer = document.getElementById('field-container');
+        this.dogElement = document.createElement('div');
+        this.dogContainer = document.getElementById('dog-container');
+    }
+
+    launchWalkoutAnimation() {
+        this.dogElement.setAttribute("id", this.dogId);
+        this.fieldContainer.appendChild(this.dogElement);
+
+        this.dogElement.addEventListener('animationend', () => {
+            this.dogElement.classList.add('found');
+        });
+
+        this.dogElement.addEventListener('animationend', () => {
+            setTimeout(() => {
+                this.dogElement.classList.add('jump');
+            }, 1000);
+        });
+
+        this.dogElement.addEventListener('animationend', () => {
+            setTimeout(() => {
+                this.dogElement.remove();
+            }, 1800);
+        });
+    }
+
+
+    showDogWithKilledDucks(killedDucks) {
+        this.dogElement.setAttribute("id", this.dogId);
+        this.dogContainer.appendChild(this.dogElement);
+
+        if (killedDucks === 1) {
+            this.dogElement.classList.add('gotOne');
+
+        } else {
+            this.dogElement.classList.add('gotTwo');
+        }
+
+
+
+
+
+    }
+
+    showDogLaugh() {
+        this.dogElement.setAttribute("id", this.dogId);
+        this.dogContainer.appendChild(this.dogElement);
+        this.dogElement.classList.add('laugh');
+    }
+}   
--- main.js
+++ main.js
@@ -4,4 +4,15 @@ playButton.addEventListener('click', createDuck);
 function createDuck() {
     let ducky = new Duck();
     ducky.spawnDuck();
+    let dog1 = new Dog("dog1");
+    dog1.launchWalkoutAnimation();
+    console.log("doggy1");
+
+    let dog2 = new Dog("dog2");
+    // dog2.showDogWithKilledDucks(1);
+
+    // dog2.showDogWithKilledDucks(2);
+
+    dog2.showDogLaugh();
+    
 }
